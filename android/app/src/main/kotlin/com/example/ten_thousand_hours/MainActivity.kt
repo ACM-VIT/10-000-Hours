a720e4a6be8750cec26168b671c8b95676d1038e
Himanshu Sharma <himanshu.sharma2021@vitstudent.ac.in>
2022-10-03 01:21:42 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ten_thousand_hours/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ten_thousand_hours
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text("Body"),
+      ),
+    );
+  }
+}
