4f4d6b80cc7c9635b226588235ef6388d5da6852
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2022-10-05 19:34:40 +0530

    feat : add taskTime screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ten_thousand_hours/providers/task_list_provider.dart';
 import 'view/screens/add_task.dart';
+import 'view/screens/task_time.dart';
 
 import 'view/screens/home_screen.dart';
 
@@ -14,6 +15,7 @@ void main() {
       routes: {
         HomeScreen.id: (context) => const HomeScreen(),
         AddTask.id: (context) => AddTask(),
+        TaskTimer.id: (context) => TaskTimer(),
       },
     ),
   ));
