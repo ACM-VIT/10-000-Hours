c4d956ecd9659097213312f68925804339ded42f
pratikagr <pratikben.1000@gmail.com>
2022-10-09 17:35:01 +0530

    added edit task screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ten_thousand_hours/providers/task_list_provider.dart';
 import 'package:ten_thousand_hours/providers/timer_provider.dart';
+import 'package:ten_thousand_hours/view/screens/edit_taskname.dart';
 import 'package:ten_thousand_hours/view/screens/splash_screen.dart';
 import 'view/screens/add_task.dart';
 import 'view/screens/timer_screen.dart';
@@ -20,10 +21,11 @@ void main() {
     ],
     child: MaterialApp(
       title: "10,000 Hours",
-      initialRoute: splashScreen.id,
+      initialRoute: EditTaskName.id,
       routes: {
         
         splashScreen.id: (context) => splashScreen(),
+        EditTaskName.id: (context) => EditTaskName(),
         HomeScreen.id: (context) => HomeScreen(),
         AddTask.id: (context) => AddTask(),
         TaskTimer.id: (context) => const TaskTimer(),
