0eef72ddd59b502a4817c413b1f02982891ab3a4
pratikagr <pratikben.1000@gmail.com>
2022-10-15 21:07:39 +0530

    updated task card

--- lib/view/widgets/task_card.dart
+++ lib/view/widgets/task_card.dart
@@ -6,23 +6,28 @@ class TaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [BoxShadow(blurRadius: 3, color: Colors.grey)],
-        color: Colors.orangeAccent,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      margin: const EdgeInsets.all(10),
-      height: 80,
-      width: 600,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(12, 16, 8, 8),
-        child: Text(
-          taskName,
-          style: const TextStyle(
-              color: Color.fromARGB(255, 57, 57, 57),
-              fontSize: 20,
-              fontWeight: FontWeight.w400),
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+      minHeight: 80,
+    ),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [BoxShadow(blurRadius: 3, color: Colors.grey)],
+          color: Colors.orangeAccent,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.all(10),
+        // height: 80.2,
+        width: 600,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 16, 8, 8),
+          child: Text(
+            taskName,
+            style: const TextStyle(
+                color: Color.fromARGB(255, 57, 57, 57),
+                fontSize: 16,
+                fontWeight: FontWeight.w400),
+          ),
         ),
       ),
     );
